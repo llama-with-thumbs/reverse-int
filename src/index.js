6a215fddfff0aafb9888fd6fef85fef1557abfc6
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-13 14:55:55 -0400

    feat: implement reverse int func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const absN = String(Math.abs(n))
+    let res = ''
+    for (let i = absN.length - 1; i >= 0; i--) {
+        res += absN[i]
+    }
+    return Number.parseInt(res)
 }
